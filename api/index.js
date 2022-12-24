9f4a7f1d8d5da83238054d8a9545b827843b9f1b
Abhishek0090 <palabhishek411@gmail.com>
2022-12-24 17:30:21 +0530

    added backend files

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import express   from "express";
+import userRoutes from "./routes/users.js"; //use .js for avoiding error
+ 
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
-}
+const app = express();
+const port = 8800;
 
-export default index
+
+app.get('/',(req,res)=>{
+  return res.json(["Hello bro"]);
+})
+
+app.use('/api/users',userRoutes);
+
+app.listen(port,()=>{
+  console.log(`Your website is running on http://localhost:${port}`);
+})
---
+++ api/routes/users.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const router = express.Router();
+
+
+
+
+router.get('/test',(req,res)=>{
+   res.send("Its workings bro")
+})
+
+
+ 
+
+
+export default router;
