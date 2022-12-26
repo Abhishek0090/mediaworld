34b4e08c805b5aefa050095245cc2a83a0e0014c
Abhishek0090 <palabhishek411@gmail.com>
2022-12-26 18:41:13 +0530

    register added

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,32 +1,64 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './register.scss';
-
+ 
 const Register = () => {
+
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+    name: ""
+  })
+
+  const [err, setErr] = useState(null);
+
+
+  const handleChange = e => {
+    setInputs(prev=>({...prev,[e.target.name]:e.target.value}))
+    console.log(inputs)
+  }
+
+
+  const register = async (e)=>{
+    e.preventDefault();
+
+    try {
+      await axios.post('http://localhost:8800/api/auth/register',inputs);
+    } catch (error) {
+        console.log(error);
+      setErr(err.response.data)
+    }
+  }
+
+  console.log(err);
+
   return (
     <div className="register">
-    <div className="card">
-    <div className='left'>
-            <h1>Register</h1>
-            <form>
-                <input type="text" placeholder='Username' />
-                <input type="email" placeholder='Email' />
-                <input type="password" placeholder='Password' />
-                <input type="text" placeholder='Name' />
-                <button>Register</button>
-            </form>
-    </div>
-        <div className='right'>
-            <h1>Abhi Sup</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro id impedit omnis  
-            </p>
-            <span>Do You have an Account</span> 
-            <Link to="/login">
-
-            <button>Login</button>
-            </Link>
+      <div className="card">
+        <div className='left'>
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder='Username' name="usename" onChange={handleChange} />
+            <input type="email" placeholder='Email' name="email" onChange={handleChange} />
+            <input type="password" placeholder='Password' name="password" onChange={handleChange} />
+            <input type="text" placeholder='Name' name="name" onChange={handleChange} />
+            <button onClick={register}>Register</button>
+          </form>
         </div>
-        
-    </div>
+        <div className='right'>
+          <h1>Abhi Sup</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro id impedit omnis
+          </p>
+          <span>Do You have an Account</span>
+          <Link to="/login">
+
+            <button>Login</button> 
+          </Link>
+        </div>
+
+      </div>
     </div>
   )
 }
